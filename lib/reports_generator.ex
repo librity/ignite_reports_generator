28cf2eed41f99b2c22b1e5b35dd9af50a38ac75c
Luis Geniole <luis.geniole@gmail.com>
2021-03-09 20:34:10 -0300

    Write ReportsGenerator tests.

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -8,6 +8,12 @@ defmodule ReportsGenerator do
     |> Enum.reduce(initialize_report(), &accumulate_fields/2)
   end
 
+  def fetch_max_from_field(report, field) when field in @fields do
+    {:ok, Enum.max_by(report[field], fn {_key, value} -> value end)}
+  end
+
+  def fetch_max_from_field(_report, _field), do: {:error, "Invalid field."}
+
   defp initialize_report, do: %{"users" => %{}, "food_items" => %{}}
 
   defp accumulate_fields(line, %{"users" => users, "food_items" => food_items} = report) do
@@ -29,10 +35,4 @@ defmodule ReportsGenerator do
 
   defp previous_or_zero(map_value) when is_nil(map_value), do: 0
   defp previous_or_zero(map_value) when is_number(map_value), do: map_value
-
-  defp fetch_max_from_field(report, field) when field in @fields do
-    {:ok, Enum.max_by(report[field], fn {_key, value} -> value end)}
-  end
-
-  defp fetch_max_from_field(_report, _field), do: {:error, "Invalid field."}
 end
--- test/reports_generator_test.exs
+++ test/reports_generator_test.exs
@@ -1,8 +1,78 @@
 defmodule ReportsGeneratorTest do
   use ExUnit.Case
-  doctest ReportsGenerator
 
-  test "greets the world" do
-    assert ReportsGenerator.hello() == :world
+  describe "build/1" do
+    test "should build and return a report from a .csv file" do
+      file_name = "report_test"
+      return = ReportsGenerator.build(file_name)
+
+      expected = %{
+        "food_items" => %{
+          "açaí" => 1,
+          "churrasco" => 2,
+          "esfirra" => 3,
+          "hambúrguer" => 2,
+          "pizza" => 2
+        },
+        "users" => %{
+          "1" => 48,
+          "10" => 36,
+          "2" => 45,
+          "3" => 31,
+          "4" => 42,
+          "5" => 49,
+          "6" => 18,
+          "7" => 27,
+          "8" => 25,
+          "9" => 24
+        }
+      }
+
+      assert expected == return
+    end
+  end
+
+  describe "fetch_max_from_field/2" do
+    test "should return the greatest buyer from 'users'" do
+      file_name = "report_test"
+      field = "users"
+
+      return =
+        file_name
+        |> ReportsGenerator.build()
+        |> ReportsGenerator.fetch_max_from_field(field)
+
+      expected = {:ok, {"5", 49}}
+
+      assert expected == return
+    end
+
+    test "should return the top seller from 'food_items'" do
+      file_name = "report_test"
+      field = "food_items"
+
+      return =
+        file_name
+        |> ReportsGenerator.build()
+        |> ReportsGenerator.fetch_max_from_field(field)
+
+      expected = {:ok, {"esfirra", 3}}
+
+      assert expected == return
+    end
+
+    test "should return an :error when field isn't valid" do
+      file_name = "report_test"
+      field = "foobar"
+
+      return =
+        file_name
+        |> ReportsGenerator.build()
+        |> ReportsGenerator.fetch_max_from_field(field)
+
+      expected = {:error, "Invalid field."}
+
+      assert expected == return
+    end
   end
 end
