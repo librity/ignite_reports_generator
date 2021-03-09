726e8208434c4f7dfea2840e4ebbd19e0c024b3d
Luis Geniole <luis.geniole@gmail.com>
2021-03-09 17:58:26 -0300

    Use reduce to calculate gross revenue.

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -2,14 +2,22 @@ defmodule ReportsGenerator do
   def build(filename) do
     "data/#{filename}.csv"
     |> File.stream!()
-    |> Enum.map(&parse_line/1)
+    |> Enum.reduce(%{}, &parse_and_add/2)
+  end
+
+  defp parse_and_add(line, report) do
+    [id, _item_name, price] = parse_line(line)
+    previous = previous_or_zero(report[id])
+    Map.put(report, id, previous + price)
   end
 
   defp parse_line(line) do
     line
     |> String.trim()
     |> String.split(",")
-    |> List.update_at(0, &String.to_integer/1)
     |> List.update_at(2, &String.to_integer/1)
   end
+
+  defp previous_or_zero(nil), do: 0
+  defp previous_or_zero(number), do: number
 end
