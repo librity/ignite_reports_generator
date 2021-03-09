a96ff0abff39dbc0a039e83fd2da6f3bd890b74b
Luis Geniole <luis.geniole@gmail.com>
2021-03-09 19:21:42 -0300

    Add food_items accumulator to report.

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -3,14 +3,31 @@ defmodule ReportsGenerator do
 
   def build(filename) do
     Parser.call(filename)
-    |> Enum.reduce(%{}, &add_to_previous/2)
+    |> Enum.reduce(initialize_report(), &accumulate_fields/2)
   end
 
-  defp add_to_previous([id, _item_name, price], report) do
-    previous = previous_or_zero(report[id])
-    Map.put(report, id, previous + price)
+  defp initialize_report, do: %{"users" => %{}, "food_items" => %{}}
+
+  defp accumulate_fields(line, %{"users" => users, "food_items" => food_items}) do
+    users = accumulate_users(line, users)
+    food_items = accumulate_food_items(line, food_items)
+
+    %{"users" => users, "food_items" => food_items}
+  end
+
+  defp accumulate_users([user_id, _item_name, price], users) do
+    previous = previous_or_zero(users[user_id])
+    Map.put(users, user_id, previous + price)
+  end
+
+  defp accumulate_food_items([_user_id, item_name, price], food_items) do
+    previous = previous_or_zero(food_items[item_name])
+    Map.put(food_items, item_name, previous + price)
   end
 
   defp previous_or_zero(nil), do: 0
   defp previous_or_zero(number), do: number
+
+  defp fetch_greatest_buyer(%{"users" => users, "food_items" => _food_items}),
+    do: Enum.max_by(users, fn {_key, value} -> value end)
 end
